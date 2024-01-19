7b0e28b3e3ef78b99010eac1747eb90271c21b96
SiyanaPetkova <s.petkova323@gmail.com>
2024-01-19 20:35:14 +0200

    Create custom 404 page.

---
+++ wp-content/themes/honeyfarm-theme/404.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<div class="about-box-main">    
+    <div class="container">
+        <div class="row">                      
+            <div class="col-lg-12">
+                <h2 class="noo-sh-title-top">Може би се загуби?</h2>
+                <h3>404 - Страницата, която търсиш, не може да бъде намерена.</h3>
+
+                <?php
+                    $image_url = get_template_directory_uri() . '/images/not-found-404.gif';
+                ?>
+
+                <img src="<?php echo esc_url( $image_url ); ?>" alt="Not Found" style="max-width: 100%; height: auto;">
+            </div>
+        </div>          
+    </div> 
+</div>
+
+<?php get_footer(); ?>
