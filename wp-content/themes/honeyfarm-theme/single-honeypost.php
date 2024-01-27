63cbd881ea47d80e84c164fe199853fad623bdff
SiyanaPetkova <s.petkova323@gmail.com>
2024-01-27 15:49:24 +0200

    Adding published date toggle with ACF.

--- wp-content/themes/honeyfarm-theme/single-honeypost.php
+++ wp-content/themes/honeyfarm-theme/single-honeypost.php
@@ -10,16 +10,25 @@
                     <div class="row">                      
                         <div class="col-lg-9">
                             <h2 class="noo-sh-title-top"><?php the_title(); ?></h2>
-                            <p><?php the_content(); ?></p>
+                            <div class="published-date">
+                                <?php
+                                    $display_date = get_field( 'show_publishdate' ); 
+
+                                    if ( $display_date ) {
+                                        echo 'Дата: ' . get_the_date();
+                                    }
+                                ?>
+                            </div>
+                            <p><?php the_content(); ?></p>                            
                         </div>
                         <div class="col-lg-3">                            
                             <?php
                                 if ( is_active_sidebar( 'recent-posts' ) ) {
                                     get_sidebar( 'recent-posts' );
                                 }
-                            ?>
-                        </div>
-                    </div>          
+                            ?>                             
+                        </div>                       
+                    </div>                           
                  </div>
             <?php endwhile; else : ?>
 
